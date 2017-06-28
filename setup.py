93170b1cdcb7b7120ea8074437931ff5d766e10a
Jorge <jbernal0019@yahoo.es>
2017-06-28 13:05:48 -0400

    Added setup for PyPI

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+
+# Make sure we are running python3.5+
+if 10 * sys.version_info[0]  + sys.version_info[1] < 35:
+    sys.exit("Sorry, only Python 3.5+ is supported.")
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+      name             =   'med2image',
+      version          =   '0.1',
+      description      =   '(Python) utility to convert medical images to jpg and png',
+      long_description =   readme(),
+      author           =   'FNNDSC',
+      author_email     =   'dev@babymri.org',
+      url              =   'https://github.com/FNNDSC/med2image',
+      packages         =   ['med2image'],
+      install_requires =   ['nibabel', 'pydicom', 'numpy', 'matplotlib', 'pillow'],
+      #test_suite       =   'nose.collector',
+      #tests_require    =   ['nose'],
+      #scripts          =   [],
+      license          =   'MIT',
+      zip_safe         =   False
+)
