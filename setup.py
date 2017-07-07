abf45313a6a0a8361ba19f6b1671b057a5fe6d47
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-07-07 10:55:15 -0400

    Update to 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'med2image',
-      version          =   '1.0.0',
+      version          =   '1.0.1',
       description      =   '(Python) utility to convert medical images to jpg and png',
       long_description =   readme(),
       author           =   'FNNDSC',
