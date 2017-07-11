0a0daf131e6a01efbfc58ff8f092ab9810a0dc9b
Jorge <jbernal0019@yahoo.es>
2017-07-11 14:41:55 -0400

    v1.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'med2image',
-      version          =   '1.0.2',
+      version          =   '1.0.3',
       description      =   '(Python) utility to convert medical images to jpg and png',
       long_description =   readme(),
       author           =   'FNNDSC',
